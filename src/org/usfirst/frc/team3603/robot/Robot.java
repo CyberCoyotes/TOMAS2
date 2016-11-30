2a6aada90dbdc673953237aac84d9b6147920b54
Wiley Coyotes <cybercoyotes@gmail.com>
2016-11-30 16:55:38 -0500

    Added encoder base code.

--- src/org/usfirst/frc/team3603/robot/Robot.java
+++ src/org/usfirst/frc/team3603/robot/Robot.java
@@ -8,9 +8,11 @@
 package org.usfirst.frc.team3603.robot;
 
 import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -29,6 +31,10 @@ public class Robot extends IterativeRobot {
 	RobotDrive mainDrive2 = new RobotDrive(left2, right2);
 	RobotDrive mainDrive3 = new RobotDrive(left1, right1, left2, right2);
 	
+	Timer timer = new Timer();
+	
+	Encoder jerrie = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
+	
     public void robotInit() {
     	gyro.reset();
     }
@@ -40,13 +46,16 @@ public class Robot extends IterativeRobot {
     }
 
     public void teleopPeriodic() {
+    	timer.start();
+    	jerrie.reset();
     	while (isOperatorControl() && isEnabled()) {
     		double magnitude1 = xbox.getRawAxis(1);
     		double magnitude2 = xbox.getRawAxis(5);
-    		double all = magnitude1+magnitude2;
-    		if(all>0.25 || all<-0.25) { 
-    			mainDrive.tankDrive(-xbox.getRawAxis(1), -xbox.getRawAxis(5));
-    			mainDrive2.tankDrive(-xbox.getRawAxis(1), -xbox.getRawAxis(5));
+    		double all = magnitude1 + magnitude2;
+    		
+    		if(all>=0.25||all<=0.25)
+		    	mainDrive.tankDrive(-magnitude1, -magnitude2);
+		    	mainDrive2.tankDrive(-magnitude1, -magnitude2);
     		}
     		while(xbox.getRawButton(4)) {
     			mainDrive.tankDrive(0.5, 0.5);
@@ -79,8 +88,21 @@ public class Robot extends IterativeRobot {
     				mainDrive2.tankDrive(-0.75, 0.75);
     			}
     			start = 0;
+    		
     		}
-    	}
+    		int count = jerrie.get();
+    		double distance = jerrie.getRaw();
+    		double distance2 = jerrie.getDistance();
+    		double rate = jerrie.getRate();
+    		
+    		
+    		SmartDashboard.putNumber("distance in inches", rate);
+    		
+    		
+    		
+    		
+    		
+    		
     }
     
     public void testPeriodic() {
