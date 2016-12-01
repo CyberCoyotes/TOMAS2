2bf6918223526e97e0d8125395b665c5f4c2c511
Wiley Coyotes <cybercoyotes@gmail.com>
2016-12-01 16:49:42 -0500

    Not working. TOMAS may have something fried. And it's not chicken.

--- src/org/usfirst/frc/team3603/robot/Robot.java
+++ src/org/usfirst/frc/team3603/robot/Robot.java
@@ -12,14 +12,13 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Robot extends IterativeRobot {
 	
-	Joystick xbox = new Joystick(1);
+	Joystick xbox = new Joystick(0);
 	Victor left1 = new Victor(1);
 	Victor right1 = new Victor(2);
 	Victor left2 = new Victor(3);
@@ -27,11 +26,8 @@ public class Robot extends IterativeRobot {
 	
 	AnalogGyro gyro = new AnalogGyro(0);
 	
-	RobotDrive mainDrive = new RobotDrive(left1, right1);
-	RobotDrive mainDrive2 = new RobotDrive(left2, right2);
-	RobotDrive mainDrive3 = new RobotDrive(left1, right1, left2, right2);
-	
-	Timer timer = new Timer();
+	RobotDrive mainDrive = new RobotDrive(left2, left1, right2, right1); 
+	                                    //(frontLeft, rearLeft, frontRight rearRight)
 	
 	Encoder jerrie = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
 	
@@ -46,67 +42,25 @@ public class Robot extends IterativeRobot {
     }
 
     public void teleopPeriodic() {
-    	timer.start();
     	jerrie.reset();
     	while (isOperatorControl() && isEnabled()) {
-    		double magnitude1 = xbox.getRawAxis(1);
-    		double magnitude2 = xbox.getRawAxis(5);
-    		double all = magnitude1 + magnitude2;
+    		double mag1 = xbox.getRawAxis(1);
+    		double mag2 = xbox.getRawAxis(5);
+    		double all = mag1 + mag2;
     		
-    		if(all>=0.25||all<=0.25)
-		    	mainDrive.tankDrive(-magnitude1, -magnitude2);
-		    	mainDrive2.tankDrive(-magnitude1, -magnitude2);
+    		if(all>=0.25||all<=0.25) {
+    			mainDrive.tankDrive(-mag1, -mag2);
     		}
-    		while(xbox.getRawButton(4)) {
-    			mainDrive.tankDrive(0.5, 0.5);
-    			mainDrive2.tankDrive(0.5, 0.5);
+    		if(xbox.getRawButton(1)) {
+    			mainDrive.tankDrive(0.75, 0.75);
     		}
-    		while(xbox.getRawButton(1)) {
-    			mainDrive.tankDrive(-0.5,-0.5);
-    			mainDrive2.tankDrive(-0.5,-0.5);
-    		}
-    		while(xbox.getRawButton(3)) {
-    			mainDrive.tankDrive(0.75, -0.75);
-    			mainDrive2.tankDrive(-0.75, 0.75);
-    		}
-    		while(xbox.getRawButton(2)) {
-    			mainDrive.tankDrive(-0.75, 0.75);
-    			mainDrive2.tankDrive(0.75, -0.75);
-    		}
-    		if(xbox.getRawButton(8)) {
-    			double start = gyro.getAngle();
-    			while(gyro.getAngle()<=start+90) {
-    				mainDrive.tankDrive(0.75, -0.75);
-    				mainDrive2.tankDrive(0.75, -0.75);
-    			}
-    			start = 0;
-    		}
-    		if(xbox.getRawButton(7)) {
-    			double start = gyro.getAngle();
-    			while(gyro.getAngle()>=start-90) {
-    				mainDrive.tankDrive(-0.75, 0.75);
-    				mainDrive2.tankDrive(-0.75, 0.75);
-    			}
-    			start = 0;
     		
-    		}
-    		int count = jerrie.get();
-    		double distance = jerrie.getRaw();
-    		double distance2 = jerrie.getDistance();
-    		double rate = jerrie.getRate();
-    		
-    		
-    		SmartDashboard.putNumber("distance in inches", rate);
-    		
-    		
-    		
-    		
-    		
-    		
+    		double rate = gyro.getAngle();
+    		SmartDashboard.putNumber("ratethingy", rate);
+    	}
     }
     
     public void testPeriodic() {
     
     }
-    
 }
