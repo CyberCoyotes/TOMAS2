655cd0ae1d2c92e622bb8f74dc25cd514d596b07
Connor Williams <minisolarclown@gmail.com>
2016-12-15 18:15:44 -0500

    Changes
    Got rid of unused code. Currently, it is set up for large joysticks
    ONLY. Buttons will not do anything. Holonomic drive. Braking is a
    mechanical thing. To put the robot in brake mode:
    
    1.Go to speed controller
    2.Locate a tiny switch on the side
    3.Switch it to "B" for brake. "C" is coast.
    4.Do the same for all of the speed controllers.
    
    Added a text file to write what the current controls are.
    
    -Connor W.

--- src/org/usfirst/frc/team3603/robot/Robot.java
+++ src/org/usfirst/frc/team3603/robot/Robot.java
@@ -21,8 +21,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 	
-	Joystick xbox1 = new Joystick(2);
-	Joystick xbox2 = new Joystick(3);
+	Joystick joy1 = new Joystick(2);
+	Joystick joy2 = new Joystick(3);
+	
 	Victor left1 = new Victor(1);
 	Victor right1 = new Victor(2);
 	Victor left2 = new Victor(3);
@@ -33,17 +34,9 @@ public class Robot extends IterativeRobot {
 	                             //(frontLeft, rearLeft, frontRight rearRight)
 	
 	Encoder jerrie = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
-	DoubleSolenoid thrower = new DoubleSolenoid(0, 1);
-	Value out = DoubleSolenoid.Value.kForward;
-	Value in = DoubleSolenoid.Value.kForward;
 	Timer timer = new Timer();
 	
-	AnalogInput right = new AnalogInput(1);
-	AnalogInput mid = new AnalogInput(2);
-	AnalogInput left = new AnalogInput(3);
-	
     public void robotInit() {
-    	
     	gyro.reset();
     	gyro.setSensitivity(0.5);
     	jerrie.reset();
@@ -53,6 +46,7 @@ public class Robot extends IterativeRobot {
     	jerrie.setMinRate(10);
     	jerrie.setDistancePerPulse(0.25);
     	jerrie.setSamplesToAverage(7);
+    	
     }
     
 	public void autonomousInit() {
@@ -64,122 +58,22 @@ public class Robot extends IterativeRobot {
     public void teleopPeriodic() {
     	jerrie.reset();
     	while (isOperatorControl() && isEnabled()) {
-	    	if(xbox1.getRawButton(1) || xbox1.getRawButton(2) || xbox1.getRawButton(3) || xbox1.getRawButton(4) || xbox1.getRawButton(5) || xbox1.getRawButton(6) || xbox1.getRawButton(7) || xbox1.getRawButton(8) || xbox1.getRawButton(9) || xbox1.getRawButton(10) || 
-	    			xbox2.getRawButton(1) || xbox2.getRawButton(2) || xbox2.getRawButton(3) || xbox2.getRawButton(4) || xbox2.getRawButton(5) || xbox2.getRawButton(6) || xbox2.getRawButton(7) || xbox2.getRawButton(8) || xbox2.getRawButton(9) || xbox2.getRawButton(10) ||
-	    			xbox1.getRawAxis(1) >= 0.05 || xbox1.getRawAxis(2) >= 0.05 || xbox1.getRawAxis(3) >= 0.05 || xbox1.getRawAxis(4) >= 0.05 || xbox1.getRawAxis(5) >= 0.05 || xbox1.getRawAxis(6) >= 0.05 ||
-	    			xbox2.getRawAxis(1) >= 0.05 || xbox2.getRawAxis(2) >= 0.05 || xbox2.getRawAxis(3) >= 0.05 || xbox2.getRawAxis(4) >= 0.05 || xbox2.getRawAxis(5) >= 0.05 || xbox2.getRawAxis(6) >= 0.05 ||
-	    			xbox1.getRawAxis(1) <= -0.05 || xbox1.getRawAxis(2) <= -0.05 || xbox1.getRawAxis(3) <= -0.05 || xbox1.getRawAxis(4) <= -0.05 || xbox1.getRawAxis(5) <= -0.05 || xbox1.getRawAxis(6) <= -0.05 ||
-	    			xbox2.getRawAxis(1) <= -0.05 || xbox2.getRawAxis(2) <= -0.05 || xbox2.getRawAxis(3) <= -0.05 || xbox2.getRawAxis(4) <= -0.05 || xbox2.getRawAxis(5) <= -0.05 || xbox2.getRawAxis(6) <= -0.05) {
+	    	if(joy1.getRawButton(1) || joy1.getRawButton(2) || joy1.getRawButton(3) || joy1.getRawButton(4) || joy1.getRawButton(5) || joy1.getRawButton(6) || joy1.getRawButton(7) || joy1.getRawButton(8) || joy1.getRawButton(9) || joy1.getRawButton(10) || 
+	    			joy2.getRawButton(1) || joy2.getRawButton(2) || joy2.getRawButton(3) || joy2.getRawButton(4) || joy2.getRawButton(5) || joy2.getRawButton(6) || joy2.getRawButton(7) || joy2.getRawButton(8) || joy2.getRawButton(9) || joy2.getRawButton(10) ||
+	    			joy1.getRawAxis(1) >= 0.05 || joy1.getRawAxis(2) >= 0.05 || joy1.getRawAxis(3) >= 0.05 || joy1.getRawAxis(4) >= 0.05 || joy1.getRawAxis(5) >= 0.05 || joy1.getRawAxis(6) >= 0.05 ||
+	    			joy2.getRawAxis(1) >= 0.05 || joy2.getRawAxis(2) >= 0.05 || joy2.getRawAxis(3) >= 0.05 || joy2.getRawAxis(4) >= 0.05 || joy2.getRawAxis(5) >= 0.05 || joy2.getRawAxis(6) >= 0.05 ||
+	    			joy1.getRawAxis(1) <= -0.05 || joy1.getRawAxis(2) <= -0.05 || joy1.getRawAxis(3) <= -0.05 || joy1.getRawAxis(4) <= -0.05 || joy1.getRawAxis(5) <= -0.05 || joy1.getRawAxis(6) <= -0.05 ||
+	    			joy2.getRawAxis(1) <= -0.05 || joy2.getRawAxis(2) <= -0.05 || joy2.getRawAxis(3) <= -0.05 || joy2.getRawAxis(4) <= -0.05 || joy2.getRawAxis(5) <= -0.05 || joy2.getRawAxis(6) <= -0.05) {
 	    		/**********************
 	    		*** DRIVER CONTROLS ***
 	    		**********************/
-	    		double mag1 = -Math.pow(xbox1.getRawAxis(1), 3);
-	    		double mag2 = -Math.pow(xbox2.getRawAxis(1), 3);
-	    		//
-	    		//if(Math.abs(mag1)>=0.1 || Math.abs(mag2)>=0.1) {
-	    		//	mainDrive.tankDrive(mag1, mag2);
-	    		//}
 	    		
-	    		while(xbox1.getRawButton(1)) {
-	    			mainDrive.tankDrive(-0.75, -0.75);
-	    		}
-	    		double max = gyro.getAngle()+5;
-				double min = gyro.getAngle()-5;
-	    		while(xbox1.getRawButton(2)) {
-	    			if(gyro.getAngle()>=min&&gyro.getAngle()<=max) {
-	    				left1.set(-0.75);
-	    				left2.set(0.75);
-	    				right1.set(-0.75);
-	    				right2.set(0.75);
-	    			}
-	    			if(gyro.getAngle()>max) {
-	    				left1.set(-0.6);
-	    				left2.set(0.9);
-	    				right1.set(-0.9);
-	    				right2.set(0.6);
-	    			}
-	    			if(gyro.getAngle()<min) {
-	    				left1.set(-0.9);
-	    				left2.set(-0.6);
-	    				right1.set(-0.6);
-	    				right2.set(0.9);
-	    			}
-	    		}
-	    		while(xbox1.getRawButton(3)) {
-	    			if(gyro.getAngle()>=min && gyro.getAngle()<=max) {
-	    				left1.set(-0.75);
-	    				left2.set(0.75);
-	    				right1.set(0.75);
-	    				right2.set(-0.75);
-	    			}
-	    			if(gyro.getAngle()>max) {
-	    				left1.set(-0.6);
-	    				left2.set(0.9);
-	    				right1.set(0.9);
-	    				right2.set(-0.6);
-	    			}
-	    			if(gyro.getAngle()<min) {
-	    				left1.set(-0.9);
-	    				left2.set(0.6);
-	    				right1.set(0.6);
-	    				right2.set(-0.9);
-	    			}
-	    		}
-	    		while(xbox1.getRawButton(4)) {
-	    			mainDrive.tankDrive(0.75, 0.75);
-	    		}
-	    		/*
-	    		while(xbox1.getRawAxis(2)>=0.2) {
-	    			mainDrive.tankDrive(-xbox1.getRawAxis(2), xbox1.getRawAxis(2));
-	    		}
-	    		while(xbox1.getRawAxis(3)>=0.2) {
-	    			mainDrive.tankDrive(xbox1.getRawAxis(3), -xbox1.getRawAxis(3));
-	    		}
-	    		*/
-	    		/****************************
-	    		 *** MANIPULATOR CONTROLS *** Reserved for testing at the moment
-	    		 ****************************/
-	    		double x = -Math.pow(xbox1.getRawAxis(0), 3);
-	    		double y = -Math.pow(xbox1.getRawAxis(1), 3);
-	    		double rot = -Math.pow(xbox2.getRawAxis(0), 3);
+	    		double x = -Math.pow(joy1.getRawAxis(0), 3);
+	    		double y = -Math.pow(joy1.getRawAxis(1), 3);
+	    		double rot = -Math.pow(joy2.getRawAxis(0), 3);
 	    		
 	    		if(Math.abs(x)>=0.1 || Math.abs(y)>=0.1 || Math.abs(rot)>=0.1) {
 	    			mainDrive.mecanumDrive_Cartesian(x, rot, y, gyro.getAngle());
-	    		}
-	    		
-	    		if(xbox2.getRawButton(5)) {
-	    			thrower.set(out);
-	    		} else {
-	    			thrower.set(in);
-	    		}
-	    		
-	    		if(xbox2.getRawButton(1)) {
-	    			left1.set(-0.75);
-	    		}
-	    		if(xbox2.getRawButton(2)) {
-	    			left2.set(-0.75);
-	    		}
-	    		if(xbox2.getRawButton(3)) {
-	    			right1.set(-0.75);
-	    		}
-	    		if(xbox2.getRawButton(4)) {
-	    			right2.set(-0.75);
-	    		}
-	    		//
-	    		/**Sensor reading section**/
-	    		while(xbox2.getRawButton(6)) {
-		    		int r = right.getValue();
-		    		int m = mid.getValue();
-		    		int l = left.getValue();
-		    		if(r>=4000) {
-		    			mainDrive.tankDrive(0.5, -0.5);
-		    		}
-		    		if(m>=4000) {
-		    			mainDrive.tankDrive(0, 0);
-		    		}
-		    		if(l>=4000) {
-		    			mainDrive.tankDrive(-0.5, 0.5);
-		    		}
 	    		}
 	    		
 	    		if(gyro.getAngle()>=360) {
@@ -195,11 +89,22 @@ public class Robot extends IterativeRobot {
 	    		SmartDashboard.putNumber("Time", timer.get());
     	
 	    	} else {
-	    		mainDrive.tankDrive(0, 0);
-	            left1.stopMotor();
-	            left2.stopMotor();
-	            right1.stopMotor();
-	            right2.stopMotor();
+	    		if(gyro.getAngle()>=360) {
+	    			gyro.reset();
+	    		}
+	    		if(gyro.getAngle()<=-360) {
+	    			gyro.reset();
+	    		}
+	    		
+	    		SmartDashboard.putNumber("Rate", jerrie.getRate());
+	    		SmartDashboard.putNumber("Distance", jerrie.getDistance());
+	    		SmartDashboard.putNumber("Gyro Value", gyro.getAngle());
+	    		SmartDashboard.putNumber("Time", timer.get());
+	    		
+	    		left1.set(0);
+	    		left2.set(0);
+	    		right1.set(0);
+	    		right2.set(0);
 	    	}
     	}
     }
